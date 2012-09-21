a74c2d8320627019b3cc9f26d8924c931f231a99
Wanga <jwanga@WangaJamesMBP.local>
2012-09-21 12:02:39 -0400

    discrete span sizing steps

--- app/list/calculateSpan-filter.js
+++ app/list/calculateSpan-filter.js
@@ -5,6 +5,10 @@ Application.Filters.filter('calculateSpan', function() {
 		var output, size;
 		
 		size = Math.ceil(12/input);
+		
+		if(size < 12 && size > 6){
+		  output
+		}
 		output = size < 2 ? 2 : size;
 		
 		return output;
