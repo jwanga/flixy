528cb262088d3908028bee1b400280e8e399dafc
Wanga <jwanga@WangaJamesMBP.local>
2012-09-24 10:14:26 -0400

    added new node server.
    use /scripts/server.js instead of /scripts/web-server.js

--- app/list/calculateSpan-filter.js
+++ app/list/calculateSpan-filter.js
@@ -6,22 +6,13 @@ Application.Filters.filter('calculateSpan', function() {
 		
 		size = Math.ceil(12/input);
 		
-<<<<<<< HEAD
-		if(size < 12 && size > 6){
-		  output
-		}
-=======
-		/*
 		if(size < 12 && size >=6){
 			output = 6;
 		}else if(size < 6 && size >=4){
 			output = 4;
 		}else if(size < 4 ){
 			output = 2;
-		}*/
-		
->>>>>>> apn calculation chages
-		output = size < 2 ? 2 : size;
+		}
 		
 		return output;
 	}
