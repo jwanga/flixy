bbc4f6cc7082c0b1dc7b08dbbc3d99d57ee7f69f
Adam Parrish <aparrish@neosavvy.com>
2012-09-24 10:19:40 -0400

    Adding in the html5push state business.

--- app/application/application.js
+++ app/application/application.js
@@ -10,9 +10,75 @@ Application.Directives = angular.module('application.directives', []);
 
 // Declare app level module which depends on filters, and services
 angular.module('application', ['application.filters', 'application.services', 'application.directives']).
-  config(['$routeProvider', function($routeProvider) {
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    function getParameterByName (name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.search);
+        if (results == null) {
+            return "";
+        }
+        else {
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+    }
+
+    var isPushState = getParameterByName("pushState");
+    console.log("Pushstate is: " + isPushState);
+    if (isPushState === undefined || isPushState === null || isPushState === "") {
+        isPushState = true;
+    }
+
+    var routeConfig = {};
+    if (isPushState == true) {
+        console.log("Acting like push state should be active");
+        routeConfig = {
+            html5PushState: true, // dev: false, prod: true
+            historyPrefix: ""
+        };
+    }
+    else {
+        console.log("Acting like push state should not be active");
+        routeConfig = {
+            html5PushState: false, // dev: false, prod: true
+            historyPrefix: "#"
+        };
+    }
+
+    $locationProvider.html5Mode(routeConfig.html5PushState);
   
     $routeProvider.when('/', {templateUrl: 'list/list-partial.html', controller: Application.Controllers.ListController});
     $routeProvider.when('/:query', {templateUrl: 'list/list-partial.html', controller: Application.Controllers.ListController});
     $routeProvider.otherwise({templateUrl: 'errors/404-partial.html'});
-  }]);
+  }]).
+  run(['$location','$rootScope', function($location, $scope) {
+
+    $scope.isPushState = $location.search().pushState;
+
+    $scope.routeConfig = {};
+
+    console.log("app.run() pushState=" + $scope.isPushState);
+
+    if ($scope.isPushState === undefined || $scope.isPushState === null ||
+        $scope.isPushState === "") {
+        $scope.isPushState = true;
+    }
+
+    if ($scope.isPushState == true) {
+        console.log("Acting like push state should be active");
+        $scope.routeConfig = {
+            html5PushState: true, // dev: false, prod: true
+            historyPrefix: ""
+        };
+    }
+    else {
+        console.log("Acting like push state should not be active");
+        $scope.routeConfig = {
+            html5PushState: false, // dev: false, prod: true
+            historyPrefix: "#"
+        };
+    }
+
+}]);
--- app/list/calculateSpan-filter.js
+++ app/list/calculateSpan-filter.js
@@ -1,28 +1,16 @@
 'use strict'
 
 Application.Filters.filter('calculateSpan', function() {
-	return function(input) {
-		var output, size;
-		
-		size = Math.ceil(12/input);
-		
-<<<<<<< HEAD
-		if(size < 12 && size > 6){
-		  output
-		}
-=======
-		/*
-		if(size < 12 && size >=6){
-			output = 6;
-		}else if(size < 6 && size >=4){
-			output = 4;
-		}else if(size < 4 ){
-			output = 2;
-		}*/
-		
->>>>>>> apn calculation chages
-		output = size < 2 ? 2 : size;
-		
-		return output;
-	}
+    return function(input) {
+        var output, size;
+
+        size = Math.ceil(12/input);
+
+        if(size < 12 && size > 6){
+            output
+        }
+        output = size < 2 ? 2 : size;
+
+        return output;
+    }
 });
